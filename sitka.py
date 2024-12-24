1abd48891bc1eddc3a1c6436c0112495363c69fa
msartuc <mserdarartuc@gmail.com>
2024-12-25 00:08:28 +0300

    map

--- sitka.py
+++ sitka.py
@@ -11,19 +11,23 @@ header_row = next(reader)
 print(header_row)
 
 #extracting high data
-dates, highs = [], []
+dates, highs, lows = [], [], []
 for row in reader:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
-    high = [int(row[4])]
+    high = int(row[4])
+    low = int(row[5])
     dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 
 plt.style.use('classic')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, color='blue')
+ax.plot(dates, highs, color='blue', alpha=0.5)
+ax.plot(dates, lows, color='green', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
-ax.set_title("Daily High Temperatures, 2021", fontsize=24)
+ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
