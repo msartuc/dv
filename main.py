2f04b4c3cfa6ff63ed3293a72480720d4c776b63
msartuc <mserdarartuc@gmail.com>
2024-12-24 21:33:45 +0300

    fplot

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-# spearhead
-print("it starts here")
+import matplotlib.pyplot as plt
+
+squares = [1, 4, 9, 16, 25]
+fig, ax = plt.subplots()
+ax.plot(squares, linewidth=3)
+ax.set_title("Squares", fontsize = 24)
+ax.set_xlabel("Value", fontsize = 14)
+ax.set_ylabel("Square values", fontsize = 14)
+ax.tick_params(labelsize = 24)
+
+plt.show()
