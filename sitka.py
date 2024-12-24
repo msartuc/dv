8c6103ec9988d7f7b5a8ddb520434c19b18d92cd
msartuc <mserdarartuc@gmail.com>
2024-12-24 22:58:39 +0300

    narray cast

---
+++ sitka.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import csv
+import matplotlib.pyplot as plt
+
+path = Path('weather_data/sitka_weather_07-2021_simple.csv')
+lines = path.read_text().splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+print(header_row)
+
+#extracting high data
+highs=[]
+for row in reader:
+    high = [int(row[4])]
+    highs.append(high)
+print(highs)
+print(type(highs))
+plt.style.use('classic')
+fig, ax = plt.subplots()
+ax.plot(highs, color='red')
+
+# Format plot.
+ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(labelsize=16)
+plt.show()
