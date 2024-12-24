ff6a908e06747eb5e511b293caf6fe0238bb960e
msartuc <mserdarartuc@gmail.com>
2024-12-24 23:28:47 +0300

    ts

--- sitka.py
+++ sitka.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 import csv
 import matplotlib.pyplot as plt
+from datetime import datetime
 
-path = Path('weather_data/sitka_weather_07-2021_simple.csv')
+path = Path('weather_data/sitka_weather_2021_simple.csv')
 lines = path.read_text().splitlines()
 
 reader = csv.reader(lines)
@@ -10,19 +11,21 @@ header_row = next(reader)
 print(header_row)
 
 #extracting high data
-highs=[]
+dates, highs = [], []
 for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = [int(row[4])]
+    dates.append(current_date)
     highs.append(high)
-print(highs)
-print(type(highs))
+
 plt.style.use('classic')
 fig, ax = plt.subplots()
-ax.plot(highs, color='red')
+ax.plot(dates, highs, color='blue')
 
 # Format plot.
-ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_title("Daily High Temperatures, 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(labelsize=16)
 plt.show()
